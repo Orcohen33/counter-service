59774ec84307e2a396409f4595a51a8eb97fe0bc
Orcohen33 <orcohen3322@gmail.com>
2025-08-12 09:06:33 +0300

    move application to `/app` folder

--- app/counter-service.py
+++ app/counter-service.py
@@ -1,15 +1,21 @@
 #!flask/bin/python
+import os
 from flask import Flask, request, request_started
 
 app = Flask(__name__)
 counter = 0
-@app.route('/', methods=["POST", "GET"])
+
+
+@app.route("/", methods=["POST", "GET"])
 def index():
     global counter
     if request.method == "POST":
-        counter+=1
+        counter += 1
         return "Hmm, Plus 1 please "
     else:
         return str(f"Our counter is: {counter} ")
-if __name__ == '__main__':
-    app.run(debug=True,port=80,host='0.0.0.0')
+
+
+if __name__ == "__main__":
+    port = os.environ.get("PORT", 5000)
+    app.run(debug=True, port=port, host="0.0.0.0")
